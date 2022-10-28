254e339bcc18981c031108eae73d0a3b9bd397a2
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-10-28 12:16:36 +0100

    colours for radio buttons

--- src/Components/PaymentOptions/index.js
+++ src/Components/PaymentOptions/index.js
@@ -6,17 +6,66 @@ import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 
 export default function PaymentOptions() {
+
+    // const controlProps = (item) => ({
+	// 		checked: selectedValue === item,
+	// 		onChange: handleChange,
+	// 		value: item,
+	// 		name: "color-radio-button-demo",
+	// 		inputProps: { "aria-label": item },
+	// 	});
+        
 	return (
 		<FormControl>
-			<FormLabel sx={{color: "#D2212E"}} id="demo-radio-buttons-group-label">Select Payment Method:</FormLabel>
+			<FormLabel
+				sx={{
+					color: "white",
+					"&.Mui-focused": {
+						color: "white",
+					},
+				}}
+				id="demo-radio-buttons-group-label"
+			>
+				Select Payment Method:
+			</FormLabel>
 			<RadioGroup
 				aria-labelledby="demo-radio-buttons-group-label"
-				defaultValue="female"
 				name="radio-buttons-group"
+				sx={{
+					color: "white",
+					"&.Mui-focused": {
+						color: "white",
+					},
+				}}
 			>
-				<FormControlLabel value="Card" control={<Radio />} label="Card" />
-				<FormControlLabel value="PayPal" control={<Radio />} label="PayPal" />
-
+				<FormControlLabel
+					value="Card"
+					control={
+						<Radio
+							sx={{
+								color: "#D2212E",
+								"&.Mui-checked": {
+									color: "#D2212E",
+								},
+							}}
+						/>
+					}
+					label="Card"
+				/>
+				<FormControlLabel
+					value="PayPal"
+					control={
+						<Radio
+							sx={{
+								color: "#D2212E",
+								"&.Mui-checked": {
+									color: "#D2212E",
+								},
+							}}
+						/>
+					}
+					label="PayPal"
+				/>
 			</RadioGroup>
 		</FormControl>
 	);
