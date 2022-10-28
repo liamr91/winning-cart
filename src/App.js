71aebff97562f0d6d4cf20600ed726f8f0482386
Holly <holly.fanthorpe@iceland.co.uk>
2022-10-28 11:48:54 +0100

    product list added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const productList = [
+  { id: 1, name: 'Monster Munch', price: 0.50, quantity: 2, paid: false }, 
+  { id: 2, name: 'Chicken Nuggets', price: 2.50, quantity: 1, paid: false }, 
+  { id: 3, name: 'Pizza', price: 3, quantity: 1, paid: false },
+  { id: 4, name: 'Burger', price: 2.50, quantity: 1, paid: false },
+  { id: 5, name: 'Hash browns', price: 1.20, quantity: 2, paid: false }
+]
+
+
 function App() {
   return (
     <div className="App">
