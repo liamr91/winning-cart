d40d14736cfbc92765d9f72f8e64e8af545d8eb3
Jordan Flash <flash.jordan.3@gmail.com>
2022-10-28 11:49:50 +0100

    its not looking great

---
+++ src/Components/ItemList/index.js
@@ -0,0 +1,29 @@
+import { React, useState, useEffect } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Checkbox from "@mui/material/Checkbox";
+import Avatar from "@mui/material/Avatar";
+
+export default function ItemList() {
+  const [checked, setChecked] = useState([1]);
+
+  //code from mui
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  return <List></List>;
+}
