4f07e05c4ad6445b143a9bf9e162d19cf1ea9e3e
Amaan Memon <amaan_memon@hotmail.co.uk>
2022-10-28 13:37:09 +0100

    looks cool now merged

--- src/Components/PayNowButton/index.js
+++ src/Components/PayNowButton/index.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
+import React from "react";
+import styled from "styled-components";
+
+const PayNow = styled.button`
+  width: 5rem;
+  height: 2rem;
+  border-radius: 10px;
+  border: 2.5px solid #d2212e;
+  background-color: #d2212e;
+`;
+const Split = styled.button`
+  width: 5rem;
+  height: 2rem;
+  border-radius: 10px;
+  background-color: white;
+  border: 2.5px solid #d2212e;
+`;
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 1rem;
+  gap: 2rem;
+  text-align: center;
+`;
 
 export default function PaymentButtons() {
-        return (
-						<Stack spacing={2} direction="row">
-							<Button variant="contained">Pay Total</Button>
-							<Button variant="outlined">Split Payment</Button>
-						</Stack>
-				);
-    }
-
-
+  return (
+    <>
+      <ButtonContainer>
+        <PayNow>Pay Now</PayNow>
+        <Split>Split</Split>
+      </ButtonContainer>
+    </>
+  );
+}
